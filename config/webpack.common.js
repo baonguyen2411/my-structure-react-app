84c9e93b05b9065093f5f0f2417aeeaaed577871
BaoNG <nbao5471@gmail.com>
2021-12-17 09:42:49 +0700

    [Implement] - Implement service worker

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
@@ -49,7 +50,15 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       inject: true,
+      title: 'My Structure React App',
       template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/manifest.json' },
+        { from: 'src/assets/images/logo-192.png' },
+        { from: 'src/assets/images/logo-512.png' },
+      ],
     }),
   ],
 };
