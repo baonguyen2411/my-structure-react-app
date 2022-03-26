ed10a155e57827ae792dbf3ec974c58d628caff4
BaoNG <nbao5471@gmail.com>
2022-03-26 14:08:14 +0700

    [Update] - Update webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -37,7 +37,7 @@ module.exports = {
     ],
   },
   resolve: {
-    modules: ['node_modules', 'src'],
+    modules: ['node_modules'],
     extensions: ['*', '.js', '.jsx', '.json'],
     mainFields: ['browser', 'jsnext:main', 'main'],
     alias: {
