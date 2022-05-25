d04c7450381ce78d7e302c8dac8a9012baa04b9e
BaoNG <nbao5471@gmail.com>
2022-05-25 22:00:47 +0700

    [Config] - Config tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
-  plugins: [autoprefixer, tailwindcss],
+  plugins: {
+    tailwindcss,
+    autoprefixer,
+    ...(process.env.NODE_ENV === 'production' ? { cssnano } : {}),
+  },
 };
